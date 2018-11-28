d15954e4e0e8da5fef595284f0e196c16fb8978d
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2018-11-28 08:25:54 -0700

    Add project files.

---
+++ AsyncAwaitStopwatchDemo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitStopwatchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks tasks = new Tasks();
+
+            //First, call the tasks normally, and run a stopwatch.
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            var task1 = tasks.OneSecondTask();
+            var task2 = tasks.TwoSecondTask();
+            var task3 = tasks.ThreeSecondTask();
+            var task4 = tasks.FourSecondTask();
+            
+            Task.WaitAll(task1, task2, task3, task4);
+
+            watch.Stop();
+
+            Console.WriteLine("Standard call, watch runs for " + watch.ElapsedMilliseconds + " milliseconds");
+            Console.WriteLine("Press Enter to run watches on each task.");
+
+            Console.ReadLine();
+
+            Console.WriteLine(Environment.NewLine);
+
+            //Now, add a stopwatch call to each of them.
+
+            var task1watch = new Stopwatch();
+            task1watch.Start();
+            var watchtask1 = tasks.OneSecondTask().ContinueWith(x =>
+            {
+                task1watch.Stop();
+                Console.WriteLine("Task 1 took " + task1watch.ElapsedMilliseconds + " milliseconds to run.");
+            });
+
+            var task2watch = new Stopwatch();
+            task2watch.Start();
+            var watchtask2 = tasks.TwoSecondTask().ContinueWith(x =>
+            {
+                task2watch.Stop();
+                Console.WriteLine("Task 2 took " + task2watch.ElapsedMilliseconds + " milliseconds to run.");
+            });
+
+            var task3watch = new Stopwatch();
+            task3watch.Start();
+            var watchtask3 = tasks.ThreeSecondTask().ContinueWith(x =>
+            {
+                task3watch.Stop();
+                Console.WriteLine("Task 3 took " + task3watch.ElapsedMilliseconds + " milliseconds to run.");
+            });
+
+            var task4watch = new Stopwatch();
+            task4watch.Start();
+            var watchtask4 = tasks.FourSecondTask().ContinueWith(x =>
+            {
+                task4watch.Stop();
+                Console.WriteLine("Task 4 took " + task4watch.ElapsedMilliseconds + " milliseconds to run.");
+            });
+
+            Task.WaitAll(watchtask1, watchtask2, watchtask3, watchtask4);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsyncAwaitStopwatchDemo/Tasks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitStopwatchDemo
+{
+    public class Tasks
+    {
+        public async Task OneSecondTask()
+        {
+            await Task.Delay(2000);
+            return;
+        }
+
+        public async Task FourSecondTask()
+        {
+            await Task.Delay(10000);
+            return;
+        }
+
+        public async Task TwoSecondTask()
+        {
+            await Task.Delay(5000);
+            return;
+        }
+
+        public async Task ThreeSecondTask()
+        {
+            await Task.Delay(3000);
+            return;
+        }
+    }
+}
